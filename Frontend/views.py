5dbd371eea6f28f3edf470324962f8d8aff39ea6
Thermalight <alumite10@gmail.com>
2024-01-28 15:25:28 +0100

    cast string to float in value store

--- Frontend/views.py
+++ Frontend/views.py
@@ -8,10 +8,10 @@ def index(request):
     collector_instance = worker.collector()
     value_store = collector_instance.reader.value_store
     data = {
-        "verbruikstand": value_store["verbruik stand 1"] + value_store["verbruik stand 2"],
-        "leverstand": value_store["lever stand 1"] + value_store["lever stand 2"],
-        "vermogen": value_store["verbruik"] - value_store["leveren"],
-        "gas": value_store["aardgas"]
+        "verbruikstand": float(value_store["verbruik stand 1"]) + float(value_store["verbruik stand 2"]),
+        "leverstand": float(value_store["lever stand 1"]) + float(value_store["lever stand 2"]),
+        "vermogen": float(value_store["verbruik"]) - float(value_store["leveren"]),
+        "gas": float(value_store["aardgas"])
     }
     
     return render(request, 'index.html', data)
